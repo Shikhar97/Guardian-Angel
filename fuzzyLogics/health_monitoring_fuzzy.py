628f5c0fbcd04d20a43de6a4da4fb29908815480
tejaswisr <rsama2@asu.edu>
2023-11-25 15:14:34 -0700

    health monitory inference rules updated

--- fuzzyLogics/health_monitoring_fuzzy.py
+++ fuzzyLogics/health_monitoring_fuzzy.py
@@ -8,28 +8,36 @@ def health_monitoring_system(heart_rate_val, respiratory_rate_val, steps_count_v
     steps_count = ctrl.Antecedent(np.arange(0, 10000, 1), 'steps_count')
     overall_well_being = ctrl.Consequent(np.arange(0, 100, 1), 'overall_well_being')
 
-    heart_rate['low'] = fuzz.trimf(heart_rate.universe, [0, 50, 100])
-    heart_rate['normal'] = fuzz.trimf(heart_rate.universe, [50, 100, 150])
-    heart_rate['high'] = fuzz.trimf(heart_rate.universe, [100, 150, 200])
+    heart_rate['low'] = fuzz.trimf(heart_rate.universe, [0, 60, 100])
+    heart_rate['normal'] = fuzz.trimf(heart_rate.universe, [60, 100, 140])
+    heart_rate['high'] = fuzz.trimf(heart_rate.universe, [100, 140, 200])
 
     respiratory_rate['low'] = fuzz.trimf(respiratory_rate.universe, [0, 10, 20])
     respiratory_rate['normal'] = fuzz.trimf(respiratory_rate.universe, [10, 20, 30])
     respiratory_rate['high'] = fuzz.trimf(respiratory_rate.universe, [20, 30, 40])
 
-    steps_count['low'] = fuzz.trimf(steps_count.universe, [0, 2500, 5000])
-    steps_count['normal'] = fuzz.trimf(steps_count.universe, [2500, 5000, 7500])
-    steps_count['high'] = fuzz.trimf(steps_count.universe, [5000, 7500, 10000])
+    steps_count['low'] = fuzz.trimf(steps_count.universe, [0, 3000, 5000])
+    steps_count['normal'] = fuzz.trimf(steps_count.universe, [3000, 5000, 7000])
+    steps_count['high'] = fuzz.trimf(steps_count.universe, [5000, 7000, 10000])
 
     overall_well_being['critical'] = fuzz.trimf(overall_well_being.universe, [0, 25, 50])
-    overall_well_being['physical_distress'] = fuzz.trimf(overall_well_being.universe, [25, 50, 75])
-    overall_well_being['respiratory_issues'] = fuzz.trimf(overall_well_being.universe, [50, 75, 100])
+    overall_well_being['concerning'] = fuzz.trimf(overall_well_being.universe, [25, 50, 75])
+    overall_well_being['normal'] = fuzz.trimf(overall_well_being.universe, [50, 75, 100])
 
-    rule1 = ctrl.Rule(heart_rate['high'] & respiratory_rate['high'], overall_well_being['critical'])
-    rule2 = ctrl.Rule(steps_count['low'] & heart_rate['high'], overall_well_being['physical_distress'])
-    rule3 = ctrl.Rule(respiratory_rate['low'] & heart_rate['normal'], overall_well_being['respiratory_issues'])
-    rule4 = ctrl.Rule(steps_count['high'], overall_well_being['physical_distress'])  # New rule for high steps count
+    rules = []
 
-    health_monitoring_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4])
+    rules.append(ctrl.Rule(heart_rate['high'] & respiratory_rate['high'], overall_well_being['critical']))
+    rules.append(ctrl.Rule((heart_rate['high'] | heart_rate['normal']) & steps_count['low'], overall_well_being['concerning']))
+    rules.append(ctrl.Rule(respiratory_rate['low'] & (heart_rate['normal'] | heart_rate['low']), overall_well_being['concerning']))
+    rules.append(ctrl.Rule(heart_rate['normal'] & respiratory_rate['normal'] & steps_count['normal'], overall_well_being['normal']))
+    rules.append(ctrl.Rule(steps_count['low'], overall_well_being['concerning']))
+    rules.append(ctrl.Rule(respiratory_rate['high'] & heart_rate['normal'], overall_well_being['concerning']))
+    rules.append(ctrl.Rule(heart_rate['high'] & respiratory_rate['normal'], overall_well_being['concerning']))
+    rules.append(ctrl.Rule(heart_rate['normal'] & respiratory_rate['normal'] & steps_count['high'], overall_well_being['normal']))
+    rules.append(ctrl.Rule(heart_rate['low'] & respiratory_rate['normal'] & steps_count['normal'], overall_well_being['normal']))
+    rules.append(ctrl.Rule(heart_rate['normal'] & respiratory_rate['low'] & steps_count['high'], overall_well_being['normal']))
+
+    health_monitoring_ctrl = ctrl.ControlSystem(rules)
     health_monitoring = ctrl.ControlSystemSimulation(health_monitoring_ctrl)
 
     health_monitoring.input['heart_rate'] = heart_rate_val
@@ -40,8 +48,7 @@ def health_monitoring_system(heart_rate_val, respiratory_rate_val, steps_count_v
 
     return health_monitoring.output['overall_well_being']
 
-# Example usage:
-heart_rate_input = 120
+heart_rate_input = 80
 respiratory_rate_input = 20
 steps_count_input = 8000
 
