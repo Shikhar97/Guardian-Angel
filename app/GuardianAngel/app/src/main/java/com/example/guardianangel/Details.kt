2e03e4e44e68286e5ef91f5e1f0375d0e7312c59
Shikhar Gupta <sgupt330@asu.edu>
2023-12-05 20:02:32 -0700

    Added sleep content

--- app/GuardianAngel/app/src/main/java/com/example/guardianangel/Details.kt
+++ app/GuardianAngel/app/src/main/java/com/example/guardianangel/Details.kt
@@ -3,6 +3,7 @@ package com.example.guardianangel
 
 import android.os.Bundle
 import android.text.Editable
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,14 +15,17 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.viewModels
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.textfield.MaterialAutoCompleteTextView
 import com.google.android.material.textfield.TextInputLayout
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.flow.Flow
+import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
 class Details : Fragment() {
-    private var btn: Button? = null
     private val applicationScope = CoroutineScope(SupervisorJob())
     private val database by lazy { UsersDb.AppDatabase.getDatabase(this.requireContext(), applicationScope) }
     private val repository by lazy { UsersRepository(database.userDao()) }
@@ -34,17 +38,58 @@ class Details : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val view: View = inflater.inflate(R.layout.fragment_details, container, false)
-        view.findViewById<TextInputLayout>(R.id.textField1).editText?.text = Editable.Factory.getInstance().newEditable("Shikhar")
-//        view.findViewById<TextInputLayout>(R.id.textField2).editText?.text = Editable.Factory.getInstance().newEditable("Gupta")
-        view.findViewById<TextInputLayout>(R.id.textField3).editText?.text = Editable.Factory.getInstance().newEditable(27.toString())
+        view.findViewById<TextInputLayout>(R.id.name).editText?.text = Editable.Factory.getInstance().newEditable("Shikhar Gupta")
+        view.findViewById<TextInputLayout>(R.id.age).editText?.text = Editable.Factory.getInstance().newEditable(27.toString())
 //        val items = arrayOf("Male", "Female")
 //        (view.findViewById<TextInputLayout>(R.id.textField4).editText as? MaterialAutoCompleteTextView)?.setSimpleItems(items)
 //        .text = Editable.Factory.getInstance().newEditable("Male")
-        view.findViewById<TextInputLayout>(R.id.textField8).editText?.text = Editable.Factory.getInstance().newEditable(64.toString())
-        view.findViewById<TextInputLayout>(R.id.textField9).editText?.text = Editable.Factory.getInstance().newEditable(175.toString())
+        view.findViewById<TextInputLayout>(R.id.weight).editText?.text = Editable.Factory.getInstance().newEditable(64.toString())
+        view.findViewById<TextInputLayout>(R.id.height).editText?.text = Editable.Factory.getInstance().newEditable(175.toString())
 
 
-//        btn = view.findViewById(R.id.next_fab)
+        val topAppBar = view.findViewById<MaterialToolbar>(R.id.topAppBar)
+        topAppBar.setNavigationOnClickListener {
+            // Handle navigation icon press
+        }
+
+        val bloodG = view.findViewById<TextInputLayout>(R.id.bloodg)
+        val allergy = view.findViewById<TextInputLayout>(R.id.allergy)
+        val medicalCond = view.findViewById<TextInputLayout>(R.id.medc)
+        val medic = view.findViewById<TextInputLayout>(R.id.medcs)
+
+        topAppBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.save_icon -> {
+//                    val dataToInsert = UsersDb.User(
+//                        firstName = arguments?.getString("first_name"),
+//                        lastName = arguments?.getString("last_name"),
+//                        age = arguments?.getInt("age"),
+//                        gender = arguments?.getString("gender"),
+//                        weight = arguments?.getFloat("weight"),
+//                        height = arguments?.getFloat("height"),
+//                        bloodGroup = bloodG.editText?.text.toString(),
+//                        allergy = allergy.editText?.text.toString(),
+//                        medicalCond = medicalCond.editText?.text.toString(),
+//                        medic = medic.editText?.text.toString(),
+//                    )
+//                    Log.d("Angel", dataToInsert.toString())
+//                    userViewModel.insert(dataToInsert)
+
+                    Toast.makeText(
+                        activity,
+                        "Details saved",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    true
+                }
+                R.id.help -> {
+                    // Handle more item (inside overflow menu) press
+                    true
+                }
+                else -> false
+            }
+        }
+
 //        btn?.setOnClickListener {
 //            val bundle = Bundle().apply {
 //                putString("first_name", view.findViewById<TextInputLayout>(R.id.textField1).editText?.text.toString()) // Replace with your actual variable
@@ -61,5 +106,42 @@ class Details : Fragment() {
 //            fm.beginTransaction().replace(R.id.frame_layout, fragment).commit()
 //        }
         return view
+    }
+
+    private fun getUserAttributes(userId: String="655ff2802c6a0e4de1d9a9d4"){
+        val baseUrl = "https://mc-guardian-angel-1fec5a1eb0b8.herokuapp.com/users/$userId/user_attributes"
+        val apiKey = "<api_key>"
+        // <For testing> Without API key the below code doesn't work. Reach out to aelango3@asu.edu for api key
+        val client = OkHttpClient()
+
+
+        val url = baseUrl.toHttpUrlOrNull()!!.newBuilder()
+            .addQueryParameter("user_preference", wakeupPreference)
+            .build()
+
+        val request = Request.Builder()
+            .url(url)
+            .header("X-Api-Auth", apiKey)
+            .method("GET", null)
+            .build()
+
+        println(request)
+
+        client.newCall(request).execute().use { response ->
+            if (!response.isSuccessful)
+                throw IOException("Unexpected code $response")
+            wakeUpTime = 420L
+
+            for (header in response.headers) {
+                println("${header.first}: ${header.second}")
+            }
+
+            val responseBody = response.body?.string()
+            println(responseBody)
+            val jsonResponse = parseJsonResponse(responseBody)
+            wakeUpTime = jsonResponse?.optLong("wake_up_time")!!
+        }
+        // <For testing> Comment the above code and uncomment the below code for immediate testing
+        return wakeUpTime
     }
 }
