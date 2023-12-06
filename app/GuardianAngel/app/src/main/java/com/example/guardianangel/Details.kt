2af52f094142f3eef438f32be3795340ea9330dc
Shikhar Gupta <sgupt330@asu.edu>
2023-12-06 00:31:47 -0700

    Fixed issues

--- app/GuardianAngel/app/src/main/java/com/example/guardianangel/Details.kt
+++ app/GuardianAngel/app/src/main/java/com/example/guardianangel/Details.kt
@@ -26,8 +26,8 @@ import okhttp3.Request
 class Details : Fragment() {
 
     private val gson = Gson()
-    private val API_KEY = BuildConfig.HEROKU_API_KEY
-
+    private val apiKey = BuildConfig.HEROKU_API_KEY
+    private val tag = "Angel"
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,31 +36,28 @@ class Details : Fragment() {
     ): View {
 //        val policy = ThreadPolicy.Builder().permitAll().build()
 //        StrictMode.setThreadPolicy(policy)
+        val userAttributes = getUserAttributes()
+
 
         val view: View = inflater.inflate(R.layout.fragment_details, container, false)
         view.findViewById<TextInputLayout>(R.id.name).editText?.text =
-            Editable.Factory.getInstance().newEditable("Shikhar Gupta")
+            Editable.Factory.getInstance().newEditable(userAttributes[0].toString())
         view.findViewById<TextInputLayout>(R.id.age).editText?.text =
-            Editable.Factory.getInstance().newEditable(27.toString())
+            Editable.Factory.getInstance().newEditable(userAttributes[1].toString())
 //        val items = arrayOf("Male", "Female")
 //        (view.findViewById<TextInputLayout>(R.id.textField4).editText as? MaterialAutoCompleteTextView)?.setSimpleItems(items)
 //        .text = Editable.Factory.getInstance().newEditable("Male")
         view.findViewById<TextInputLayout>(R.id.weight).editText?.text =
-            Editable.Factory.getInstance().newEditable(64.toString())
+            Editable.Factory.getInstance().newEditable(userAttributes[3].toString())
         view.findViewById<TextInputLayout>(R.id.height).editText?.text =
-            Editable.Factory.getInstance().newEditable(175.toString())
+            Editable.Factory.getInstance().newEditable(userAttributes[4].toString())
 
+        view.findViewById<TextInputLayout>(R.id.bloodg).editText?.text = Editable.Factory.getInstance().newEditable("B-")
+        view.findViewById<TextInputLayout>(R.id.allergy).editText?.text = Editable.Factory.getInstance().newEditable("None")
+        view.findViewById<TextInputLayout>(R.id.medc).editText?.text = Editable.Factory.getInstance().newEditable("Cold, Cough")
+        view.findViewById<TextInputLayout>(R.id.medcs).editText?.text = Editable.Factory.getInstance().newEditable("None")
 
         val topAppBar = view.findViewById<MaterialToolbar>(R.id.topAppBar)
-        topAppBar.setNavigationOnClickListener {
-            // Handle navigation icon press
-        }
-
-        val bloodG = view.findViewById<TextInputLayout>(R.id.bloodg)
-        val allergy = view.findViewById<TextInputLayout>(R.id.allergy)
-        val medicalCond = view.findViewById<TextInputLayout>(R.id.medc)
-        val medic = view.findViewById<TextInputLayout>(R.id.medcs)
-
         topAppBar.setOnMenuItemClickListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.save_icon -> {
@@ -95,56 +92,56 @@ class Details : Fragment() {
                 else -> false
             }
         }
-
-//        btn?.setOnClickListener {
-//            val bundle = Bundle().apply {
-//                putString("first_name", view.findViewById<TextInputLayout>(R.id.textField1).editText?.text.toString()) // Replace with your actual variable
-//                putString("last_name", view.findViewById<TextInputLayout>(R.id.textField2).editText?.text.toString()) // Replace with your actual variable
-//                putInt("age", view.findViewById<TextInputLayout>(R.id.textField3).editText?.text.toString().toInt()) // Replace with your actual variable
-//                putString("gender", view.findViewById<TextInputLayout>(R.id.textField4).editText?.text.toString()) // Replace with your actual variable
-//                putFloat("weight", view.findViewById<TextInputLayout>(R.id.textField8).editText?.text.toString().toFloat()) // Replace with your actual variable
-//                putFloat("height", view.findViewById<TextInputLayout>(R.id.textField9).editText?.text.toString().toFloat()) // Replace with your actual variable
-//            }
-//            val fragment: Fragment = MoreDetails().apply {
-//                arguments = bundle
-//            }
-//            val fm: FragmentManager = (activity as FragmentActivity).supportFragmentManager
-//            fm.beginTransaction().replace(R.id.frame_layout, fragment).commit()
-//        }
-        getUserAttributes()
         return view
     }
 
-    private fun getUserAttributes(userId: String = "655ad12b6ac4d71bf304c5eb"): String {
-        var username: String = "n/a"
+    private fun getUserAttributes(userId: String = "655ad12b6ac4d71bf304c5eb"): ArrayList<Any> {
+        val result: ArrayList<Any> = ArrayList()
         val baseUrl = "https://mc-guardian-angel-1fec5a1eb0b8.herokuapp.com/users/$userId"
         val client = OkHttpClient()
 
-//        val url = baseUrl.toHttpUrlOrNull()!!.newBuilder()
-//            .addQueryParameter("user_preference", wakeupPreference)
-//            .build()
-
         lifecycleScope.launch(Dispatchers.IO) {
-            val request = Request.Builder()
+            var request = Request.Builder()
                 .url(baseUrl)
-                .header("X-Api-Auth", API_KEY)
+                .header("X-Api-Auth", apiKey)
                 .method("GET", null)
                 .build()
-            Log.d("angel", request.toString())
-            val response = client.newCall(request).execute()
+            Log.d(tag, request.toString())
+            var response = client.newCall(request).execute()
             withContext(Dispatchers.Main) {
                 if (response.isSuccessful) {
                     val responseBody = response.body
                     val responseText = responseBody?.string()
                     val jsonObject = gson.fromJson(responseText, JsonObject::class.java)
-                    username = jsonObject.get("name").asString
-                    Log.d("angel", username)
+                    result.add(jsonObject.get("name").asString)
+                    result.add(jsonObject.get("age").asInt)
+                    result.add(jsonObject.get("gender").asString)
+                    result.add(jsonObject.get("weight").asFloat)
+                    result.add(jsonObject.get("height").asFloat)
+                } else {
+                    Log.i(tag, "Request failed with code: ${response.code}")
+                }
+                response.close()
+            }
+            request = Request.Builder()
+                .url("$baseUrl/user_attributes")
+                .header("X-Api-Auth", apiKey)
+                .method("GET", null)
+                .build()
+            Log.d(tag, request.toString())
+            response = client.newCall(request).execute()
+            withContext(Dispatchers.Main) {
+                if (response.isSuccessful) {
+                    val responseBody = response.body
+                    val responseText = responseBody?.string()
+                    val jsonObject = gson.fromJson(responseText, JsonObject::class.java)
+                    result.add(jsonObject.get("steps_count").asString)
                 } else {
                     Log.i(tag, "Request failed with code: ${response.code}")
                 }
                 response.close()
             }
         }
-        return username
+        return result
     }
 }
