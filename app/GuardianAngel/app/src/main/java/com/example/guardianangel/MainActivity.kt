8dff313956141401123235db6035bca535b7b9fe
Shikhar Gupta <sgupt330@asu.edu>
2023-12-06 21:01:30 -0700

    Added current location in card view

--- app/GuardianAngel/app/src/main/java/com/example/guardianangel/MainActivity.kt
+++ app/GuardianAngel/app/src/main/java/com/example/guardianangel/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.guardianangel
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
 import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -11,6 +16,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
@@ -21,6 +27,7 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.navigation.NavigationBarView
 import com.google.android.material.progressindicator.CircularProgressIndicator
 import kotlinx.coroutines.Dispatchers
@@ -31,11 +38,13 @@ import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.json.JSONObject
+import java.util.Locale
 
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+class MainActivity : AppCompatActivity() {
     private var bottomNavigationView: NavigationBarView? = null
     val utilfuncs = UtilFunction()
+    private var TAG = "Angel"
 
 
 //    private val applicationScope = CoroutineScope(SupervisorJob())
@@ -48,7 +57,10 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private val SERVER_API_KEY = BuildConfig.HEROKU_API_KEY
 
-    private var tag = "Angel"
+    //    private lateinit var location3Binding: ActivityLocation3Binding
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
+    private val permissionId = 2
+
     private var locations = listOf(
         Pair(33.415791, -111.925850), //McD
         Pair(33.409540, -111.916470), //Home
@@ -58,25 +70,25 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         Pair(33.429343, -111.908912), //Starbucks
         Pair(33.4218288, -111.9466686), //Oregano
     )
-    private val activityResultLauncher =
-        registerForActivityResult(
-            ActivityResultContracts.RequestMultiplePermissions()
-        )
-        { permissions ->
-            // Handle Permission granted/rejected
-            var permissionGranted = true
-            permissions.entries.forEach {
-                if (it.key in REQUIRED_PERMISSIONS && !it.value)
-                    permissionGranted = false
-            }
-            if (!permissionGranted) {
-                Toast.makeText(
-                    baseContext,
-                    "Permission request denied",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-        }
+//    private val activityResultLauncher =
+//        registerForActivityResult(
+//            ActivityResultContracts.RequestMultiplePermissions()
+//        )
+//        { permissions ->
+//            // Handle Permission granted/rejected
+//            var permissionGranted = true
+//            permissions.entries.forEach {
+//                if (it.key in REQUIRED_PERMISSIONS && !it.value)
+//                    permissionGranted = false
+//            }
+//            if (!permissionGranted) {
+//                Toast.makeText(
+//                    baseContext,
+//                    "Permission request denied",
+//                    Toast.LENGTH_SHORT
+//                ).show()
+//            }
+//        }
 
     companion object {
         private val REQUIRED_PERMISSIONS =
@@ -84,10 +96,6 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                 android.Manifest.permission.ACCESS_FINE_LOCATION,
                 android.Manifest.permission.ACCESS_COARSE_LOCATION
             ).toTypedArray()
-    }
-
-    private fun getLocation(): FusedLocationProviderClient {
-        return LocationServices.getFusedLocationProviderClient(this)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -139,11 +147,11 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         Handler(Looper.getMainLooper()).post {
             val homeFragment = supportFragmentManager.findFragmentById(R.id.frame_layout) as Home?
             if (homeFragment != null) {
-                val homeFragmentView = homeFragment?.view
+                val homeFragmentView = homeFragment.view
                 if (homeFragmentView != null) {
                     Log.d("TAG", "view not null")
-                    stepsField = homeFragmentView?.findViewById(R.id.mainStepsCount)!!
-                    progressIcon = homeFragmentView?.findViewById(R.id.mainProgressIndicator)!!
+                    stepsField = homeFragmentView.findViewById(R.id.mainStepsCount)!!
+                    progressIcon = homeFragmentView.findViewById(R.id.mainProgressIndicator)!!
 
                     progressIcon.max =
                         maxProgress
@@ -157,69 +165,15 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
             }
         }
-
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         lifecycleScope.launch {
-            var latitude = 0.0
-            var longitude = 0.0
-            if (ContextCompat.checkSelfPermission(
-                    this@MainActivity,
-                    android.Manifest.permission.ACCESS_FINE_LOCATION
-                ) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
-                    this@MainActivity,
-                    android.Manifest.permission.ACCESS_COARSE_LOCATION
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                requestPermissions()
-            }
-            val message = " You are at: \n"
             while (true) {
-                getLocation().lastLocation
-                    .addOnSuccessListener { location: Location? ->
-                        if (location != null) {
-                            latitude = location.latitude
-                            longitude = location.longitude
-                            Log.d(
-                                tag,
-                                "Current location is \n" + "lat : ${latitude}\n" +
-                                        "long : ${longitude}\n" + "fetched at ${System.currentTimeMillis()}"
-                            )
-                        } else {
-                            Log.d(
-                                tag, "Location not found"
-                            )
-                        }
-                    }
-//                val (randomLat, randomLong) = locations[random.nextInt(locations.size)]
-//                Log.d(tag, "Randomly picked pair: $randomLat, $randomLong")
-//
-//                val frag = fm.findFragmentById(R.id.frame_layout)
-//                val textView = frag?.view?.findViewById<TextView>(R.id.currentplace)
-//                val cardView = frag?.view?.findViewById<CardView>(R.id.suggestion_view)
-//                val imageView = cardView?.findViewById<ImageView>(R.id.imageView3)
-//                val mapView = frag?.view?.findViewById<MapView>(R.id.mapView)
-//
-//                mapView?.onCreate(Bundle())  // Make sure to call the necessary lifecycle methods
-//                mapView?.onResume()
-//                mapView?.getMapAsync { googleMap ->
-//                    // Now you have the GoogleMap object, and you can add a marker
-//                    val marker = MarkerOptions()
-//                        .position(LatLng(randomLat, randomLong)) // Replace with actual coordinates
-//                        .title("Marker Title") // Replace with your marker title
-//                        .snippet("Marker Snippet") // Replace with your marker snippet
-//
-//                    googleMap.addMarker(marker)
-//                    googleMap.moveCamera(
-//                        CameraUpdateFactory.newLatLngZoom(
-//                            LatLng(
-//                                randomLat,
-//                                randomLong
-//                            ), 15f
-//                        )
-//                    )
-//                }
+                getLocation()
+                delay(5000)
+            }
 
 //                if (randomLat == 33.415791 && randomLong == -111.925850) {
-//                    Log.d(tag, "You are at McDonalds")
+//                    Log.d(TAG, "You are at McDonalds")
 //                    textView?.text = "$message Starbucks\n As you have cold"
 //                    val coffeeType =
 //                        utilfuncs.getSuggestion("starbucks", "none", "none", "diabetic")
@@ -231,7 +185,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 //                    }
 //
 //                } else if (randomLat == 33.4218288 && randomLong == -111.9466686) {
-//                    Log.d(tag, "You are at Oregano's Pizza")
+//                    Log.d(TAG, "You are at Oregano's Pizza")
 //                    textView?.text = "$message Starbucks\n As you have cold"
 //                    val coffeeType =
 //                        utilfuncs.getSuggestion("starbucks", "none", "none", "diabetic")
@@ -243,7 +197,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 //                    }
 //
 //                } else if (randomLat == 33.429343 && randomLong == -111.908912) {
-//                    Log.d(tag, "You are at Starbucks")
+//                    Log.d(TAG, "You are at Starbucks")
 //                    textView?.text = "$message Starbucks\n As you have cold"
 //                    val coffeeType =
 //                        utilfuncs.getSuggestion("starbucks", "none", "none", "diabetic")
@@ -254,13 +208,12 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 //                        imageView?.setImageResource(R.mipmap.iced_tea)
 //                    }
 //                }
-                delay(5000)
-            }
         }
     }
 
-    private fun getRecentUserAttributes(userId: String="655ad12b6ac4d71bf304c5eb") {
-        val baseUrl = "https://mc-guardian-angel-1fec5a1eb0b8.herokuapp.com/users/$userId/user_attributes/recent?count=7"
+    private fun getRecentUserAttributes(userId: String = "655ad12b6ac4d71bf304c5eb") {
+        val baseUrl =
+            "https://mc-guardian-angel-1fec5a1eb0b8.herokuapp.com/users/$userId/user_attributes/recent?count=7"
         val apiKey = SERVER_API_KEY
         val client = OkHttpClient()
 
@@ -294,23 +247,88 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
-    override fun onMapReady(googleMap: GoogleMap) {
-        Log.d(tag, "Mapready")
-        googleMap.addMarker(
-            MarkerOptions()
-                .position(LatLng(0.0, 0.0))
-                .title("Marker")
+    @SuppressLint("MissingPermission", "SetTextI18n")
+    private fun getLocation() {
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+                mFusedLocationClient.lastLocation.addOnCompleteListener(this) { task ->
+                    val location: Location? = task.result
+                    if (location != null) {
+                        val geocoder = Geocoder(this, Locale.getDefault())
+                        val list: MutableList<Address>? =
+                            geocoder.getFromLocation(location.latitude, location.longitude, 1)
+//                        Log.d(TAG, "Latitude\n${list?.get(0)?.latitude}")
+//                        Log.d(TAG, "Longitude\n${list?.get(0)?.longitude}")
+//                        Log.d(TAG, "Country Name\n${list?.get(0)?.countryName}")
+//                        Log.d(TAG, "Locality\n${list?.get(0)?.locality}")
+//                        Log.d(TAG, "Address\n${list?.get(0)?.getAddressLine(0)}")
+                        val homeFragment =
+                            supportFragmentManager.findFragmentById(R.id.frame_layout) as Home?
+                        if (homeFragment != null) {
+                            val homeFragmentView = homeFragment.view
+                            if (homeFragmentView != null) {
+                                homeFragmentView.findViewById<TextView>(R.id.card4body)!!.text = list?.get(0)?.getAddressLine(0).toString().substringBefore(",").trim()
+                                homeFragmentView.findViewById<TextView>(R.id.card4body2)!!.text = list?.get(0)?.getAddressLine(0).toString().substringAfter(", ").trim()
+
+                            }
+                        }
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Please turn on location", Toast.LENGTH_LONG).show()
+                val intent = Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        } else {
+            requestPermissions()
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager =
+            getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
         )
     }
 
-    private fun requestPermissions() {
-        activityResultLauncher.launch(REQUIRED_PERMISSIONS)
+    private fun checkPermissions(): Boolean {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+        return false
     }
 
-    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
-        ContextCompat.checkSelfPermission(
-            baseContext, it
-        ) == PackageManager.PERMISSION_GRANTED
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            permissionId
+        )
+    }
+
+    @SuppressLint("MissingSuperCall")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == permissionId) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                getLocation()
+            }
+        }
     }
 }
 
