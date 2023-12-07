a35540c7b133fe3c93abfd74b79631266b9fa505
Anjali Srivastava <anjali.pihu@gmail.com>
2023-12-07 06:38:05 -0700

    add date picker changes

--- app/GuardianAngel/app/src/main/java/com/example/guardianangel/CycleTrackingProfile.kt
+++ app/GuardianAngel/app/src/main/java/com/example/guardianangel/CycleTrackingProfile.kt
@@ -1,11 +1,13 @@
 
 package com.example.guardianangel
 
+import android.content.ContentValues
 import android.os.Bundle
 import android.text.Editable
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.DateValidatorPointBackward
 import com.google.android.material.datepicker.MaterialDatePicker
 import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
@@ -19,6 +21,7 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.TimeZone
 import kotlin.Long
@@ -64,8 +67,10 @@ class CycleTrackingProfile : AppCompatActivity() {
         val constraintsBuilder =
             CalendarConstraints.Builder()
                 .setStart(startYear)
-                .setEnd(MaterialDatePicker.todayInUtcMilliseconds())
-                .setOpenAt(december)
+                .setEnd(today)
+                .setOpenAt(today)
+                .setValidator(DateValidatorPointBackward.now())
+        Log.d("daysDiff ", "Today: $today, Start Year: $startYear")
 
         val dateRangePicker =
             MaterialDatePicker.Builder.dateRangePicker()
@@ -80,24 +85,50 @@ class CycleTrackingProfile : AppCompatActivity() {
                 .setCalendarConstraints(constraintsBuilder.build())
 //                .setTheme(R.style.ThemeOverlay_MaterialComponents_MaterialCalendar)
                 .build()
+        var startDate : Long?
+        var endDate : Long?
 
+        val dateFormatUtc = SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy")
+        dateFormatUtc.timeZone = TimeZone.getTimeZone("UTC")
+        var formattedDateUtc = dateFormatUtc.format(MaterialDatePicker.thisMonthInUtcMilliseconds())
         extendedfab = findViewById(R.id.extended_fab)
         extendedfab.setOnClickListener {
             dateRangePicker.show(supportFragmentManager, "datePickerTag")
 
-            dateRangePicker.addOnPositiveButtonClickListener {
-                val startDate = dateRangePicker.selection?.first
-                val endDate = dateRangePicker.selection?.second
+            dateRangePicker.addOnPositiveButtonClickListener { selection ->
+                startDate = selection.first
+                endDate = selection.second
                 Log.d("daysDiff startDate", startDate.toString())
                 Log.d("daysDiff endDate", endDate.toString())
+
+                dateFormatUtc.timeZone = TimeZone.getTimeZone("UTC")
+                formattedDateUtc = dateFormatUtc.format(startDate)
+
+                Log.d("daysDiff formatteddate ",  formattedDateUtc.toString())
 
             }
 
         }
+        val cycleLengthInputLayout = findViewById<TextInputLayout>(R.id.cyclelength)
+        val periodLengthInputLayout = findViewById<TextInputLayout>(R.id.periodlength)
+
+        val cycleLengthValue = cycleLengthInputLayout.editText?.text.toString()
+        val periodLengthValue = periodLengthInputLayout.editText?.text.toString()
+
 
         savefab = findViewById(R.id.save_cycle_fab)
         savefab.setOnClickListener {
-            postUserCycle()
+            val dbHelper = MyDatabaseHelper(this)
+
+            val database = dbHelper.writableDatabase
+
+            val values = ContentValues().apply {
+                put("CYCLE_LENGTH", cycleLengthValue)
+                put("PERIOD_LENGTH", periodLengthValue)
+                put("LAST_PERIOD_DATE", formattedDateUtc.toString())
+            }
+            val newRowId = database.insert("cycletable", null, values)
+            database.close()
         }
 
 //            val intent = Intent(this, MyCycle::class.java)
